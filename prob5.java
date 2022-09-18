c00c266ab48a5c5126dc81cedc8c9611b344659a
rohit <rohitgiri00@yahoo.com>
2022-09-18 19:09:03 +0530

    /program to detect wether the number enter by the user is integer or not

--- prob5.java
+++ prob5.java
@@ -1,8 +1,12 @@
+//program to detect wether the number enter by the user is integer or not
+
 import java.util.*;
 public class prob5 {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-       System.out.println(sc.hasNextInt());   
+        System.out.println("enter value : ");
+        System.out.println(sc.hasNextInt());   
+
     }
 }
